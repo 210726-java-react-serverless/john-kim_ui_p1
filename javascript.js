464d5d2fec24be645de0f07ad2bcab2e47622ac4
heraldOfMechanus <jccallahan11@gmail.com>
2021-08-17 16:59:56 -0500

    Made a small javascript file and changed the welcome page to be less obscene.

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// JavaScript is a LOOSELY typed language, not an untyped language.
+document.getElementById('welcome');
+
+/*
+    JavaScript Primitive types:
+    String
+    Number
+    Boolean
+    Null (set to nil)
+    Undefined (uninitialized and has garbage data in it)
+    BigInt
+    Symbol
+
+    Structural types:
+    Object
+        - Date
+        - Map
+        - function
+ */
+
+        let a_1;
+        let b_1;
+        let c_1;
+    function a() {
+        console.log("This is a named function declaration!");
+    }
+    a();
+
+    let b = function() {
+        console.log("This is an anonymous function declaration!");
+    }
+    b();
