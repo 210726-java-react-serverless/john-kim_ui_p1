3a1cc6a6951d20f10ec2414295e80aa0fd9f044a
heraldOfMechanus <jccallahan11@gmail.com>
2021-08-19 15:01:58 -0500

    Committing files to dev for testing

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import navbarComponent from './components/navbar/navbarcomponent';
+import navbarComponent from './components/navbar/navbar.component';
 
 window.onload = () => {
     console.log(navbarComponent);
