6d18eb6a90dbd02816b98026da5dab787ae26408
heraldOfMechanus <jccallahan11@gmail.com>
2021-08-27 13:45:04 -0500

    Added two images for the student and staff portals.

--- components/landing/landing.component.js
+++ components/landing/landing.component.js
@@ -5,9 +5,17 @@ import router from '../../app.js';
 
 LandingComponent.prototype = new ViewComponent('landing');
 function LandingComponent() {
+    let studentPortalLogo;
+    let facultyPortalLogo;
+
     this.render = function() {
         LandingComponent.prototype.injectTemplate(() => {
             // Initialize any clickable site objects here!
+            studentPortalLogo = document.getElementById('student-portal');
+            facultyPortalLogo = document.getElementById('staff-portal');
+
+            studentPortalLogo.addEventListener('click', router.navigate('/login'));
+            facultyPortalLogo.addEventListener('click', router.navigate('/facLogin'));
         });
         LandingComponent.prototype.injectStyleSheet();
     }
